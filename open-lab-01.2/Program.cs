6bf49c889dbb5eca55335502bebf2ec5ab5fed7b
vladimirtischler <tischler.vladimir@gmail.com>
2019-10-10 08:12:55 +0200

    open-lab-01.2

---
+++ open-lab-01.2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace open_lab_01._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line1 = Console.ReadLine();
+            var line2 = Console.ReadLine();
+            int a = int.Parse(line1);
+            int b = int.Parse(line2);
+            int c = a + b;
+            Console.WriteLine($"Vysledok je {c}");
+        }
+    }
+}
